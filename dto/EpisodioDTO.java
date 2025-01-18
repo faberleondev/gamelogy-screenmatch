90a3e6a37bc9c9aab7c8e3a2658362cd92ac7702
Fabricio Sebastían León <129199337+faberleondev@users.noreply.github.com>
2025-01-18 18:38:54 -0300

    Delete dto directory

--- dto/EpisodioDTO.java
+++
@@ -1,6 +0,0 @@
-package com.gamelogy.screenmatch.dto;
-
-public record EpisodioDTO(Integer temporada,
-                            String titulo,
-                            Integer numeroDeEpisodio) {
-}
